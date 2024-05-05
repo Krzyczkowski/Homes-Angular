49e90fdd9dd0a538cb026e2e2ca1b8b2b67fa4a2
krzyczkowski <krzyczkowski.wiktor00@gmail.com>
2024-05-05 11:27:25 +0200

    Lesson 4
    added home component interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HousingLocation } from '../housinglocation';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -15,5 +15,16 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
